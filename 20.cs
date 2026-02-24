e05a5bafcf0493daeb99b4724f41ad2140e084c1
Vlados <bykovvlados@vk.com>
2026-02-24 09:53:14 +0400

    Update 20.cs

--- 20.cs
+++ 20.cs
@@ -4,260 +4,103 @@
 using System;
 using System.IO;
 
-class Node 
-{ 
-    public int inf;      
-    public Node next;     
-    
-    public Node(int data)
-    {
-        inf = data;
-        next = null;
-    }
-}
-
-class List
+namespace Example
 {
-    private Node head;   
-    private Node tail;    
-    private Node temp;    
-    
-    public List()
+  
+    public class List
     {
-        head = null;
-        tail = null;
-        temp = null;
-    }
-    
-    public void AddEnd(int nodeInfo)
-    {
-        Node r = new Node(nodeInfo);
-        
-        if (head == null)           
+        public class Node { public int Inf; public Node Next; }
+        private Node head;
+        private Node tail;
+        private Node temp;
+        public List()
         {
-            head = r;      
-            tail = r;               
+            head = null;
+            tail = null;
         }
-        else                         
+        public void Add(object nodeInfo)
         {
-            tail.next = r;       
-            tail = r;                   
+            Node n = new Node(nodeInfo);
+            if (head == null) head = tail = n;
+            else { n.Next = head; head = n; }
         }
-    }
-    
-    public int TakeBegin()
-    {
-        if (head == null)
-        {
-            throw new Exception("Список пуст");
-        }
-        else
-        {
-            Node r = head;      
-            head = head.next;    
-            
-            if (head == null)           
-            {
-                tail = null; 
-            }
-            
-            return r.inf;           
-        }
-    }
-    
-    public bool IsEmpty
-    {
-        get
-        {
-            return head == null;
-        }
-    }
-    
-    public Node Find(int key)
-    {
-        Node r = head;                  
-        
-        while (r != null)            
-        {
-            if (r.inf == key)         
-            {
-                break;                    
-            }
-            else
-            {
-                r = r.next;               
-            }
-        }
-        
-        return r;                       
-    }
-    
-    public void Insert(int key, int item)
-    {
-        Node r = Find(key);           
-        
-        if (r != null)                    
-        {
-            Node p = new Node(item);      
-            p.next = r.next;               
-            r.next = p;                     
-            
-            if (r == tail)                   
-            {
-                tail = p;                      
-            }
-        }
-    }
-    
-    public void Delete(int key)
-    {
-        if (head == null)                    
-        {
-            throw new Exception("Список пуст");
-        }
-        else
-        {
-            if (head.inf == key)              
-            {
-                head = head.next;               
-                
-                if (head == null)             
-                {
-                    tail = null;                 
-                }
-            }
-            else
-            {
-                Node r = head;                  
-                
-                while (r.next != null)          
-                {
-                    if (r.next.inf == key)        
-                    {
-                        r.next = r.next.next;    
-                        
-                        if (r.next == null)        
-                        {
-                            tail = r;            
-                        }
-                        
-                        break;                      
-                    }
-                    else
-                    {
-                        r = r.next;                  
-                    }
-                }
-            }
-        }
-    }
-    
-    public double Avg()
-    {
-        if (head == null) return 0;
-        
-        double sum = 0;
-        int count = 0;
-        temp = head;
-        
-        while (temp != null)
-        {
-            sum += temp.inf;
-            count++;
-            temp = temp.next;
-        }
-        
-        return sum / count;
-    }
-    
-    public void InsertAfterLess(int x, double avg)
-    {
-        temp = head;
-        
-        while (temp != null)
-        {
-            if (temp.inf < avg)
-            {
-                Node n = new Node(x);
-                n.next = temp.next;
-                temp.next = n;
-                
-                if (temp == tail)           
-                {
-                    tail = n;                
-                }
-                
-                temp = n.next;         
-            }
-            else
-            {
-                temp = temp.next;            
-            }
-        }
-    }
-    
 
-    public string Show()
-    {
-        string result = "";
-        Node r = head;                       
-        
-        while (r != null)                     
+        public double Avg()
         {
-            result += r.inf + " ";              
-            r = r.next;                          
+            if (head == null) return 0;
+            double s = 0; int k = 0;
+            temp = head;
+            while (temp != null) { s += temp.Inf; k++; temp = temp.Next; }
+            return s / k;
         }
-        
-        return result;
-    }
-    
-    public void Print()
-    {
-        Node r = head;
-        
-        while (r != null)
-        {
-            Console.Write(r.inf + " ");
-            r = r.next;
-        }
-        
-        Console.WriteLine();
-    }
-}
 
-class Program
-{
-    static void Main()
-    {
-        List L = new List();
-        
-        string[] data = File.ReadAllText("input.txt").Split(
-            new char[] { ' ', '\n', '\r' }, 
-            StringSplitOptions.RemoveEmptyEntries);
-        
-        foreach (string s in data)
+        public void InsertAfterLess(int x, double avg)
         {
-            L.AddEnd(int.Parse(s));
+            temp = head;
+            while (temp != null)
+            {
+                if (temp.Inf < avg)
+                {
+                    Node n = new Node { Inf = x, Next = temp.Next };
+                    temp.Next = n;
+                    if (temp == tail) tail = n;
+                    temp = n.Next;
+                }
+                else temp = temp.Next;
+            }
         }
-        
-        string original = L.Show();
-        
-        Console.WriteLine("Исходный список:");
-        L.Print();
-        
-        Console.Write("x = ");
-        int x = int.Parse(Console.ReadLine());
-        
-        double avg = L.Avg();
-        Console.WriteLine("Среднее арифметическое: " + avg);
-        
-        L.InsertAfterLess(x, avg);
-        
-        Console.WriteLine("Итоговый список:");
-        L.Print();
-        
-        File.WriteAllText("output.txt", original + "\n" + L.Show());
-        
-        Console.WriteLine("Результат сохранен в output.txt");
+
+        public void AddEnd(object nodeInfo)
+        {
+            Node n = new Node(nodeInfo);
+            if (head == null)
+            {
+                head = n;
+                tail = n;
+            }
+            else
+            {
+                n.Next = head;
+                head = n;
+            }
+        }
+        public void Show()
+        {
+            Node n = head; 
+            while (n != null) 
+            {   
+                Console.Write("{0} ", n.Inf);
+                n = n.Next;
+            }
+        }
+    }
+
+
+
+    public class Program
+    {
+        static void Main()
+        {
+            List list = new List();
+            using (StreamReader fileIn = new StreamReader("C:/Users/bykovvd/Desktop/Новая папка/20/input.txt"))
+            {
+                string line = fileIn.ReadToEnd();
+                string[] data = line.Split(' ');
+                foreach (string item in data)
+                {
+                    list.AddEnd(int.Parse(item));
+                }
+            }
+
+            Console.Write("x="); int x = int.Parse(Console.ReadLine());
+            list.InsertAfterLess(x, list.Avg());
+
+            using (StreamWriter fileOut = new StreamWriter("C:/Users/bykovvd/Desktop/Новая папка/20/output.txt"))
+            {
+                string line = fileOut.Show();
+
+            }
+                Console.ReadKey();
+        }
     }
 }
